8d3f32c3311cb6b4c1e102e569b9354b840d859d
Quinn <quinnledingham@gmail.com>
2025-02-03 16:52:02 -0700

    completed part 1

--- Lab1/calc.py
+++ Lab1/calc.py
@@ -8,8 +8,28 @@ def subtract(a, b):
 def dist(a, b):
     return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
 
+def slope(p1, p2):
+    return (p2[1] - p1[1]) / (p2[0] - p1[0])
+
+class Line:
+    def __init__(self, slope, point):
+        self.m = slope
+        self.b = -slope * point[0] + point[1]
+
+    @staticmethod
+    def intersection(line_a, line_b):
+        x = (line_b.b - line_a.b) / (line_a.m - line_b.m)
+        y = (line_b.m * line_a.b - line_a.m * line_b.b) / (line_b.m - line_a.m)
+        return [x, y]
+
+
+class Vector2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 fiducial_pixels = [
-    [1346, 19284],
+    [1346, 19285],
     [19170,	1478],
     [1353, 1472],
     [19163,	19291],
@@ -52,3 +72,25 @@ for coords in fiducial:
     coords[1] = pixel_spacing * coords[1]
 
 print(fiducial)
+
+m1_2 = slope(fiducial[1], fiducial[0])
+m3_4 = slope(fiducial[3], fiducial[2])
+
+print(m1_2)
+print(m3_4)
+
+line_1 = Line(m1_2, fiducial[0])
+line_2 = Line(m3_4, fiducial[2])
+
+fiducial_center = Line.intersection(line_1, line_2)
+print(fiducial_center)
+
+ppo = [-0.006, 0.006] # mm
+
+for coords in fiducial:
+    coords[0] = (coords[0] - fiducial_center[0]) - ppo[0]
+    coords[1] = (coords[1] - fiducial_center[1]) - ppo[1]
+
+print(fiducial)
+
+cfl = 153.358 # mm
