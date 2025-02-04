73097ac4aa33732f44ecf553e191b4f203ee5cb9
Quinn <44106439+quinnledingham@users.noreply.github.com>
2025-02-04 13:49:19 -0700

    small changes

--- Lab1/calc.py
+++ Lab1/calc.py
@@ -1,12 +1,10 @@
 import math
 
-def subtract(a, b):
-    x = a[0] - b[0]
-    y = a[1] - b[1]
-    return [x, y]
-
 def dist(a, b):
     return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
+
+def dist_3D(a, b):
+    return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2 + (a[2] - b[2])**2)
 
 def slope(p1, p2):
     return (p2[1] - p1[1]) / (p2[0] - p1[0])
@@ -15,6 +13,9 @@ class Line:
     def __init__(self, slope, point):
         self.m = slope
         self.b = -slope * point[0] + point[1]
+
+    def output(self):
+        print(f"y = {self.m}x + {self.b}")
 
     @staticmethod
     def intersection(line_a, line_b):
@@ -81,18 +82,15 @@ fiducial_pixels = [
 image_dim = [20456, 20488]
 
 # calculate mm / px aka pixel spacing
-dists = []
 pixel_spacing = 0
 for i in range(4):
     cali_dist = cali_dists[i]
     it = i * 2
     pix_dist = dist(fiducial_pixels[it + 1], fiducial_pixels[it])
-    dists.append(cali_dist / pix_dist)
     pixel_spacing += cali_dist / pix_dist
 pixel_spacing = pixel_spacing / 4 # mm / px
 
 fiducial = right_hand(fiducial_pixels, image_dim)
-
 scale(fiducial, pixel_spacing)
 
 m1_2 = slope(fiducial[1], fiducial[0])
@@ -101,7 +99,12 @@ m3_4 = slope(fiducial[3], fiducial[2])
 line_1 = Line(m1_2, fiducial[0])
 line_2 = Line(m3_4, fiducial[2])
 
+#line_1.output()
+#line_2.output()
+
 fiducial_center = Line.intersection(line_1, line_2)
+
+print(f"Fiducial Center: {fiducial_center}")
 
 ppo = [-0.006, 0.006] # mm
 
@@ -111,7 +114,7 @@ reduce(fiducial, fiducial_center, ppo)
 
 cp = [
     [4544, 17489], # 300, px
-    [2546, 5779]   # 304, px
+    [2547, 5778]   # 304, px
 ]
 
 rlg_cp = [
@@ -119,31 +122,26 @@ rlg_cp = [
     [-186.74, -151.54, 1093.12]  # 304, m
 ]
 
-cp = right_hand(cp, image_dim)
-scale(cp, pixel_spacing)
-reduce(cp, fiducial_center, ppo)
+cp = pixel_to_image(cp, image_dim, pixel_spacing, fiducial_center, ppo)
 
 cfl = 153.358 # mm
 
 cp_dist = dist(cp[0], cp[1]) * 1E-3   # m
-rlg_dist = dist(rlg_cp[0], rlg_cp[1]) # m 
+rlg_dist = dist_3D(rlg_cp[0], rlg_cp[1]) # m 
+print(f"Image Distance {cp_dist}, Object Distance {rlg_dist}")
 
 S = rlg_dist / cp_dist
+print(f"Scale: {S}")
 
 flying_height = S * (cfl * 1E-3) # m
-
-print(flying_height)
+print(f"Flying Height: {flying_height}")
 
 # part c: Building height estimation
 
 ict = [
     [6148, 11608], # Top, px
-    [6353, 11559]  # Bottom, px
+    [6346, 11557]  # Bottom, px
 ]
-
-#ict = right_hand(ict, image_dim)
-#scale(ict, pixel_spacing)
-#reduce(ict, fiducial_center, ppo)
 
 ict = pixel_to_image(ict, image_dim, pixel_spacing, fiducial_center, ppo)
 
@@ -152,4 +150,4 @@ rb = dist(ict[1], [0, 0])
 
 h = ((flying_height * 1E3) * (rt - rb)) / rt # mm
 
-print(h * 1E-3)
+print(f"ICT Height: {h * 1E-3}")
