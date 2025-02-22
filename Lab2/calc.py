90d0c4be90178e5a130074b5ad8198aa527e58f6
Quinn <quinnledingham@gmail.com>
2025-02-22 16:07:18 -0700

    outputing tables for report

--- Lab2/calc.py
+++ Lab2/calc.py
@@ -2,8 +2,24 @@ import pandas as pd
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import copy
 
 from docx import Document
+
+'''
+def dist_3D(a, b):
+    return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2 + (a[2] - b[2])**2)
+
+longest_dist = 0
+for a in object_coords:
+    for b in object_coords:
+        if a is b:
+            continue
+        dist = dist_3D(a, b)
+        if dist > longest_dist:
+            longest_dist = dist
+            print(f"A: {a}, B: {b}")
+'''
 
 def load_data(data, low, high):
     load = []
@@ -17,18 +33,13 @@ def load_data(data, low, high):
     return load
 
 def right_hand(a, image_dim):
-    output = []
     for coords in a:
         x = coords[0] - (image_dim[0]/2)
         y = (image_dim[1]/2) - coords[1]
-        output.append([x, y])
-    return output
 
-def right_hand_array(a, image_dim):
-    output = []
-    for elements in a:
-        output.append(right_hand(elements, image_dim))
-    return output
+        coords[0] = x
+        coords[1] = y
+
 
 def mean(a):
     sum = [0, 0]
@@ -53,52 +64,30 @@ def std(a):
     sum[1] = math.sqrt(sum[1]/(len(a) - 1))
     return sum
 
-def print_array(a):
-    for v in a:
-        print(v)
+def array_3d_to_2d(a):
+    output = []
 
-doc = Document()
+    for v1 in a:
+        e = []
+        for v2 in v1:
+            for v3 in v2:
+                e.append(v3)
+        output.append(e)
 
-def array_to_word_table(array, name, type):
+    return output
+
+def concat_2d(a, b):
+    return [row1 + row2 for row1, row2 in zip(a, b)]
+
+# print a 2d array to a word table
+def array_to_word_table(array, name, type, decimals=2):
     doc.add_paragraph(name)
-    table = doc.add_table(rows=len(array), cols=len(array[0])*2)
+    table = doc.add_table(rows=len(array), cols=len(array[0]))
 
     for i, row in enumerate(array):
         for j, cell in enumerate(row):
-            index = j * 2
-            table.cell(i, index).text = str(type(round(cell[0], 2)))
-            table.cell(i, index + 1).text = str(type(round(cell[1], 2)))
+            table.cell(i, j).text = str(type(round(cell, decimals)))
 
-def pool(input, image_dim, out):
-    data = {key: 0 for key in input.keys()}
-
-    for key in input:
-        input[key] = right_hand_array(input[key], image_dim)
-        array_to_word_table(input[key], f"{key}_{out}", int)
-        print(f"{key}")
-        print(input[key])
-
-    for i, key in enumerate(input):
-        a = input[key]
-        values = []
-        ms = []
-        stds = []
-        print(f"{key}:")
-        for v in a:
-            m = mean(v)
-            s = std(v)
-            print(f"mean: {m}, std: {s}")
-            values.append(m)
-            ms.append([m])
-            stds.append([s])
-
-        data[key] = values
-
-        array_to_word_table(ms, f"mean_{key}_{out}", float)
-        array_to_word_table(stds, f"std_{key}_{out}", float)
-
-    return data
-    
 def design_matrix(fiducial):
     n = len(fiducial)
     A = np.zeros((2 * n, 6))
@@ -138,8 +127,20 @@ def RMS(a):
     
     return math.sqrt(sum/len(a))
 
+def affine_transformation(data, x_hat):
+    for coords in data:
+        x = (x_hat[0] * coords[0] + x_hat[1] * coords[1]) + x_hat[2]
+        y = (x_hat[3] * coords[0] + x_hat[4] * coords[1]) + x_hat[5]
+
+        coords[0] = x
+        coords[1] = y
+
 # data is the fiducials in a [[]]
-def affine_transformation(data, t_data):
+
+def get_affine_transformation_parameters(data, t_data, tag):
+    print(f"\n{tag}:")
+    print(data)
+
     l = []
     for coords in t_data:
         l.append(coords[0])
@@ -152,28 +153,179 @@ def affine_transformation(data, t_data):
     print(f"x_hat: {x_hat}")
     # a b x c d y
 
+    # displaying residuals in stats and graphs
+
     residuals = A @ x_hat - l
     print(f"residuals: {residuals}")
 
-    output = []
-    for coords in data:
-        x = (x_hat[0] * coords[0] + x_hat[1] * coords[1]) + x_hat[2]
-        y = (x_hat[3] * coords[0] + x_hat[4] * coords[1]) + x_hat[5]
-        output.append([x, y])
-
     # convert 1D-array into 2D-array
     r = []
+    r_3d = []
     for i in range(len(residuals)):
         if i % 2 == 0:
-            r.append([residuals[i], residuals[i + 1]])
+            coord = [residuals[i], residuals[i + 1]]
+            r.append(coord)
+            r_3d.append([coord])
 
     #residual_plot(t_data, residuals)
 
     r = np.array(r)
     print(f"RMS x: {RMS(r[:, 0])}, y: {RMS(r[:, 1])}")
 
-    return output
+    array_to_word_table(r, f"residual_{tag}", float, decimals=4)
 
+    return x_hat
+
+class Camera:
+    def __init__(self):
+        self.ppo = [-0.006, 0.006] # mm
+
+        self.k1 = -0.1528e-7
+        self.k2 = 0.5256e-12
+        self.k3 = 0
+
+        self.p1 = 0.1346e-6
+        self.p2 = 0.1224e-7
+
+        self.f = 153.358 # focal length (mm)
+
+        self.fiducial_marks = [
+            [-105.997, -105.995],
+            [106.004, 106.008],
+            [-106.000, 106.009],
+            [106.012, -105.995],
+            [-112.000, 0.007],
+            [112.006, 0.007],
+            [0.005, 112.007],
+            [0.002, -111.998]
+        ]
+
+class Image:
+    def __init__(self, camera, tag, dim_x, dim_y, data):
+        self.camera = camera
+        self.tag = tag
+        self.dim = [dim_x, dim_y]
+        self.all_data = data
+
+    def pool(self):
+        self.data = {key: 0 for key in self.all_data.keys()}
+
+        # applying right-handed transformation
+        for key in self.all_data:
+            for v in self.all_data[key]:
+                right_hand(v, self.dim)
+
+            array_to_word_table(array_3d_to_2d(self.all_data[key]), f"{key}_{self.tag}", int)
+            print(f"{key}_{self.tag}\n{self.all_data[key]}")
+
+        # calculating mean and std
+        for i, key in enumerate(self.all_data):
+            a = self.all_data[key]
+            values = []
+
+            table = []
+
+            print(f"{key}:")
+            for v in a:
+                m = mean(v)
+                s = std(v)
+                print(f"mean: {m}, std: {s}")
+                values.append(m)
+                table.append([m[0], m[1], s[0], s[1]])
+
+            self.data[key] = values
+
+            array_to_word_table(table, f"mean__std_{key}_{self.tag}", float)
+
+    def get_affine_transformation_parameters(self):
+        self.x_hat = get_affine_transformation_parameters(self.data['fiducials'], self.camera.fiducial_marks, self.tag)
+
+    def apply_transformation(self):
+        for key in self.data:
+            affine_transformation(self.data[key], self.x_hat)
+
+    def principal_point_offest(self, coords):
+        coords[0] = (coords[0] - self.camera.ppo[0])
+        coords[1] = (coords[1] - self.camera.ppo[1])
+
+    def radial_lens_correction(self, coords):
+        x, y = coords
+
+        r = math.sqrt(x**2 + y**2)
+        dr = ((self.camera.k1 * r**3) + (self.camera.k2 * r**5) + (self.camera.k3 * r**7))
+        x_rad = -x * (dr / r)
+        y_rad = -y * (dr / r)
+
+        self.rad_table.append([r, dr, x_rad, y_rad])
+
+        return x_rad, y_rad
+
+    def decentering_lens_correction(self, coords):
+        x, y = coords
+
+        r = math.sqrt(x**2 + y**2)
+        x_dec = -(self.camera.p1 * (r**2 + 2 * x**2)) + (2 * self.camera.p2 * x * y)
+        y_dec = -(self.camera.p2 * (r**2 + 2 * y**2)) + (2 * self.camera.p1 * x * y)
+
+        self.dec_table.append([r, x_dec, y_dec])
+
+        return x_dec, y_dec
+
+    # a = [], h = [], H = int
+    def atmospheric_refraction_correction(self, coords, K):
+        x, y = coords
+        
+        r = math.sqrt(x**2 + y**2)
+        x_atm = -x * K * (1 + (r**2/self.camera.f**2))
+        y_atm = -y * K * (1 + (r**2/self.camera.f**2))
+
+        self.atm_table.append([r, x_atm, y_atm])
+
+        return x_atm, y_atm
+
+    def apply_corrections(self, h, H):
+        self.rad_table = []
+        self.dec_table = []
+        self.atm_table = []
+
+        self.transformed = []
+        self.pp_corrected = []
+        self.rad_corrected = []
+        self.dec_corrected = []
+        self.atm_corrected = []
+
+        self.measurements = []
+
+        h = h * 1E-3 # km
+        H = H * 1E-3 # km
+        K = ((2410 * H)/(H**2 - 6*H + 250)) - ((2410*h)/(h**2 - 6*h + 250)) * (h/H)
+        K = K * 1E-6
+        print(f"K: {K}")
+
+        for key in self.data:
+            for coords in self.data[key]:
+
+                transformed_x, transformed_y = coords
+                self.principal_point_offest(coords)
+                pp_x, pp_y = coords
+
+                x_rad, y_rad = self.radial_lens_correction(coords)
+                x_dec, y_dec = self.decentering_lens_correction(coords)
+                x_atm, y_atm = self.atmospheric_refraction_correction(coords, K)
+
+                rad_x, rad_y = pp_x + x_rad, pp_y + y_rad
+                dec_x, dec_y = rad_x + x_dec, rad_y + y_dec
+                atm_x, atm_y = dec_x + x_atm, dec_y + y_atm
+
+                test_x = pp_x + x_rad + x_dec + x_atm
+                test_y = pp_y + y_rad + y_dec + y_atm
+
+                if (test_x != atm_x or test_y != atm_y):
+                    print("PROBLEM HERE: adding corrections\n")
+
+                self.measurements.append([transformed_x, transformed_y, pp_x, pp_y, rad_x, rad_y, atm_x, atm_y])
+
+doc = Document()
 df = pd.read_excel('data.xlsx')
 
 data = []
@@ -192,37 +344,26 @@ og_28 = {
     "tie": load_data(data, 38, 44),
 }
 
-image_dim_27 = [20448, 20480]
-image_dim_28 = [20462, 20494]
+camera = Camera()
+
+images = [
+    Image(camera, "image_27", 20448, 20480, og_27),
+    Image(camera, "image_28", 20462, 20494, og_28)
+]
 
 # part b
 
-data_27 = pool(og_27, image_dim_27, "27")
-data_28 = pool(og_28, image_dim_28, "28")
-
-doc.save("build/output.docx")
+images[0].pool()
+images[1].pool()
 
 # part c
 
 # fiducial marks from certificate
-reseaux = [
-    [-105.997, -105.995],
-    [106.004, 106.008],
-    [-106.000, 106.009],
-    [106.012, -105.995],
-    [-112.000, 0.007],
-    [112.006, 0.007],
-    [0.005, 112.007],
-    [0.002, -111.998]
-]
+images[0].get_affine_transformation_parameters()
+images[1].get_affine_transformation_parameters()
 
-print("\nimage 27:")
-print(data_27["fiducials"])
-print(affine_transformation(data_27['fiducials'], reseaux))
-
-print("\nimage 28")
-print(data_28['fiducials'])
-print(affine_transformation(data_28['fiducials'], reseaux))
+images[0].apply_transformation()
+images[1].apply_transformation()
 
 # part d
 
@@ -264,55 +405,12 @@ test_data_reseaux = [
     [-110,	110],
 ]
 print("\nlecture data:")
-print(affine_transformation(test_data, test_data_reseaux))
+x_hat_test = get_affine_transformation_parameters(test_data, test_data_reseaux, "test")
+affine_transformation(data, x_hat_test)
 
 # part e
 
-ppo = [-0.006, 0.006] # mm
-
-k1 = -0.1528E-7
-k2 = 0.5256E-12
-k3 = 0
-
-p1 = 0.1346E-6
-p2 = 0.1224E-7
-
-f = 0.153358 # focal length (m)
-
-def principal_point_offest(a):
-    for coords in a:
-        coords[0] = (coords[0] - ppo[0])
-        coords[1] = (coords[1] - ppo[1])
-
-def radial_lens_correction(a):
-    for coords in a:
-        x, y = coords
-        r = math.sqrt(x**2 + y**2)
-        corr = ((k1 * r**2) + (k2 * r**4) + (k3 * r**6))
-        x_rad = x * corr
-        y_rad = y * corr
-
-        coords[0] = x + x_rad
-        coords[1] = y + y_rad
-
-def decentering_lens_correction(a):
-    for coords in a:
-        x, y = coords
-        r = math.sqrt(x**2 + y**2)
-        x_dec = p1 * (r**2 + 2 * x**2) + 2 * p2 * x * y
-        y_dec = p2 * (r**2 + 2 * y**2) + 2 * p1 * x * y
-
-        coords[0] = x + x_dec
-        coords[1] = y + y_dec
-
-# a = [], h = [], H = int
-def atmospheric_refraction_correction(a, h, H):
-    for coords in a:
-        K = ((2410 * H)/(H**2 - 6*H + 250)) - ((2410*h)/(h**2 - 6*h + 250)) * (h/H)
-        x, y = coords
-        r = math.sqrt(x**2 + y**2)
-        x_atm = x * K * (1 + (r**2/f**2))
-        y_atm = y * K * (1 + (r**2/f**2))
+print("\n\npart e")
 
 object_coords = [
     [-399.28, -679.72, 1090.96],
@@ -331,19 +429,34 @@ for coords in object_coords:
     avg_h = avg_h + z
 avg_h = avg_h / len(object_coords)
 
+flying_height = 751.4637599031875 # flying height from Lab 1
+H = flying_height + avg_h
+
 print(avg_h)
 
-'''
-def dist_3D(a, b):
-    return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2 + (a[2] - b[2])**2)
+images[0].apply_corrections(avg_h, H)
+images[1].apply_corrections(avg_h, H)
 
-longest_dist = 0
-for a in object_coords:
-    for b in object_coords:
-        if a is b:
-            continue
-        dist = dist_3D(a, b)
-        if dist > longest_dist:
-            longest_dist = dist
-            print(f"A: {a}, B: {b}")
-'''
+array_to_word_table(concat_2d(images[0].rad_table, images[1].rad_table), f"radial_lens", float, decimals=4)
+array_to_word_table(concat_2d(images[0].dec_table, images[1].dec_table), f"decentering_lens", float, decimals=4)
+array_to_word_table(concat_2d(images[0].atm_table, images[1].atm_table), f"atmospheric_refraction", float, decimals=4)
+
+final_table = []
+for i in range(len(images[0].measurements)):
+    new_row = []
+    for j in range(len(images[0].measurements[0])):
+        if j % 2 == 0:
+            new_row.append(images[0].measurements[i][j])
+            new_row.append(images[0].measurements[i][j + 1])
+            new_row.append(images[1].measurements[i][j])
+            new_row.append(images[1].measurements[i][j + 1])
+
+    final_table.append(new_row)
+
+print(images[0].measurements)
+#print(images[1].measurements)
+#print(final_table)
+array_to_word_table(final_table, f"measurements", float, decimals=3)
+
+doc.save("build/output.docx")
+
--- Lab2/test.py
+++ Lab2/test.py
@@ -1,22 +1,38 @@
-from docx import Document
+import math
 
-def array_to_word_table(array, filename="output.docx"):
-    doc = Document()
-    table = doc.add_table(rows=len(array), cols=len(array[0]))
+f = 152
 
-    for i, row in enumerate(array):
-        for j, cell in enumerate(row):
-            table.cell(i, j).text = str(cell)
+x = 59.043
+y = 72.392
 
-    doc.save(filename)
-    print(f"Word document '{filename}' created successfully.")
+h = 0.3
+H = 3
+K = ((2410 * H)/(H**2 - 6*H + 250)) - ((2410*h)/(h**2 - 6*h + 250)) * (h/H)
+#K = 29.7088
+K = K * 1E-6
 
-# Example usage:
-data = [
-    ["Name", "Age", "City"],
-    ["Alice", 30, "New York"],
-    ["Bob", 25, "Los Angeles"],
-    ["Charlie", 35, "Chicago"]
-]
+r = math.sqrt(x**2 + y**2)
 
-array_to_word_table(data)
+d_r = -K*(r + (r**3/f**2))
+d_d = K * (r/f)
+print(d_d)
+
+alpha = math.atan(r/f)
+print(alpha)
+
+r_prime = f * math.tan(alpha + d_d)
+
+print(r_prime)
+
+print("Solutions:")
+print((r_prime / r) * x)
+print((r_prime / r) * y)
+
+
+x_atm = -x * K * (1 + (r**2/f**2))
+y_atm = -y * K * (1 + (r**2/f**2))
+
+#print(x_atm)
+#print(y_atm)
+print(x + x_atm)
+print(y + y_atm)
