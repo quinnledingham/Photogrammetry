feb0d39062a80f7f7301b176920bf3d6e5948342
Quinn <quinnledingham@gmail.com>
2025-03-09 23:34:33 -0600

    Outputting tables to word document

--- Lab3/lab3_calc.py
+++ Lab3/lab3_calc.py
@@ -110,6 +110,7 @@ def relative_orientation(left, right, baseline):
             std_devs = np.sqrt(np.diag(cov))  # Standard deviations of parameters
             corr_matrix = cov / np.outer(std_devs, std_devs)
             print("Correlation Matrix of Estimated Parameters:\n", corr_matrix)
+            array_to_word_table(corr_matrix.tolist(), "Correlation Matrix", float, decimals=4)
             break
 
     parameters = np.array(parameters).ravel()
@@ -152,13 +153,12 @@ def space_intersection_point(left, right, base):
 
     # Calculating Y_Parallax
     y_parallax = new_ry - new_ly
-    print(f"Y_PARALLAX: { y_parallax }")
 
     if not math.isclose(new_lx, new_rx): print(f"XmL ({new_lx}) != XmR ({new_rx})")
     mean_y = (new_ly + new_ry) / 2
     if not math.isclose(new_lz, new_rz): print(f"ZmL ({new_lz}) != ZmR ({new_rz})")
 
-    return [new_lx, mean_y, new_lz]
+    return [new_lx, mean_y, new_lz], y_parallax
 
 def space_intersection(left, right, baseline, parameters):
     # apply the transformation matrix to the right image points
@@ -177,11 +177,27 @@ def space_intersection(left, right, baseline, parameters):
         }))
         model_right.append([float(result[0]), float(result[1]), float(result[2])])
 
+    y_parallaxs = []
     model_coordinates = []
     for i in range(len(left)):
-        model_coordinates.append(space_intersection_point(left[i], model_right[i], [baseline, parameters[0], parameters[1]]))
+        coordinates, y_parallax = space_intersection_point(left[i], model_right[i], [baseline, parameters[0], parameters[1]])
+        model_coordinates.append(coordinates)
+        y_parallaxs.append(y_parallax)
 
-    print(model_coordinates)
+    array_to_word_table([y_parallaxs], "Y Parallax", float, decimals=4)
+
+    print(f"Y Parallaxs {y_parallaxs}")
+    print(f"Model Coordinates {model_coordinates}")
+    return model_coordinates
+
+# print a 2d array to a word table
+def array_to_word_table(array, name, value_type, decimals=2):
+    doc.add_paragraph(name)
+    table = doc.add_table(rows=len(array), cols=len(array[0]))
+
+    for i, row in enumerate(array):
+        for j, cell in enumerate(row):
+            table.cell(i, j).text = str(round(cell, decimals))
 
 doc = Document()
 
@@ -194,7 +210,18 @@ class Data:
         self.tie = []
         self.index_map = {}
 
-df = pd.read_excel('data.xlsx')
+# Define the data
+data = {
+    "ID": [1, 2, 3, 4, 5, 6, 7, 8, 100, 102, 104, 105, 200, 201, 202, 203, "T1", "T2", "T3", "T4", "T5", "T6"],
+    "x1": [-106, 106.004, -106.002, 106.017, -111.995, 112.003, 0.011, 0.004, -9.59, -2.413, 18.943, 90.379, 18.149, 44.598, -7.657, 52.691, -10.105, 94.369, -10.762, 90.075, -9.489, 85.42],
+    "y1": [-106.004, 105.999, 106.006, -105.982, -0.002, -0.018, 112.01, -112.01, 96.218, -5.995, -81.815, -91.092, 109.575, 7.473, -49.112, -93.178, 15.011, -4.092, -104.711, -91.378, 96.26, 103.371],
+    "x2": [-105.991, 106.011, -106.011, 106.004, -111.999, 112.003, 0.015, 0.01, -105.469, -95.081, -72.547, -1.357, -77.826, -48.846, -98.855, -38.936, -103.829, 0.868, -100.169, -1.607, -105.395, -9.738],
+    "y2": [-105.995, 106.006, 105.996, -106.003, -0.003, -0.006, 112.007, -112.005, 98.736, -4.848, -79.764, -86.95, 113.405, 10.131, -48.068, -90.079, 16.042, -0.022, -103.14, -87.253, 98.706, 109.306]
+}
+
+df = pd.DataFrame(data)
+
+#df = pd.read_excel('data.xlsx')
 
 # Define control point IDs
 control_points = {100, 102, 104, 105, 200, 201, 202, 203}
@@ -220,11 +247,19 @@ for index, row in df.iterrows():
         image_27.tie.append((x27, y27))
         image_28.tie.append((x28, y28))
 
+print("\n\nLab Data\n")
 parameters = relative_orientation(image_27.tie, image_28.tie, base_distance)
-space_intersection(image_27.tie, image_28.tie, base_distance, parameters)
-space_intersection(image_27.control, image_28.control, base_distance, parameters)
+array_to_word_table([parameters], "Parameters", float, decimals=4)
+
+tie_points = space_intersection(image_27.tie, image_28.tie, base_distance, parameters)
+array_to_word_table(tie_points, "Tie Points", float, decimals=4)
+
+control_points = space_intersection(image_27.control, image_28.control, base_distance, parameters)
+array_to_word_table(control_points, "Control Points", float, decimals=4)
 
 # Verification
+print("\n\nTest Data\n")
+
 focal_length = 152.150 # mm
 
 test_data_left = [
@@ -245,7 +280,15 @@ test_data_right = [
     [8.492, -68.873]   # 50
 ]
 
-p_parameters = relative_orientation(test_data_left, test_data_right, base_distance)
-#print(p_parameters)
+# 
+test_parameters = relative_orientation(test_data_left, test_data_right, base_distance)
+array_to_word_table([test_parameters], "Test Parameters", float, decimals=4)
+
+# used to test if the space intersection is working
 target_parameters = [5.0455, 2.1725, math.radians(0.4392),  math.radians(1.508), math.radians(3.1575)]
-space_intersection(test_data_left, test_data_right, base_distance, p_parameters)
+
+test_points = space_intersection(test_data_left, test_data_right, base_distance, test_parameters)
+array_to_word_table(test_points, "Test Points", float, decimals=4)
+
+# printing answers out to a word document
+doc.save("output.docx")
